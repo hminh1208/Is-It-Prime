23615d4676fbb7d1671696cd176cede8a407a4f8
hminh1208 <hminh1208@gmail.com>
2014-11-12 00:22:28 +0700

    add to repo

--- Is It Prime/ViewController.swift
+++ Is It Prime/ViewController.swift
@@ -10,6 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var number: UITextField!
+    
+    @IBOutlet weak var message: UILabel!
+    
+    @IBAction func pressed(sender: AnyObject) {
+        
+        var num = number.text.toInt()
+        
+        if (num != nil){
+            if (num < 1){
+                message.text = "Please enter a positive number"
+            }else{
+                if(number == 1){
+                    message.text = "1 is not a prime"
+                }else{
+                    for var i = 2; i < num; i++ {
+                        if (num! % i == 0){
+                            message.text="\(num!) is not prime"
+                            return;
+                        }
+                    }
+                    message.text = "\(num!) is prime"
+                }
+            }
+        }else{
+            message.text = "Please enter a number!"
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
